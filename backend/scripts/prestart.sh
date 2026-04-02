95124384d3bb36840a596e9120cddd4e29a3f3cf
vijaya Pathikonda <vijaya.raghavendra@srmtech.com>
2026-04-02 17:42:49 +0530

    SRMD-17-backend issues
    SRMD-17: backend issues

--- backend/scripts/prestart.sh
+++ backend/scripts/prestart.sh
@@ -3,15 +3,4 @@
 set -e
 set -x
 
-if [ "$SKIP_PRESTART_DB" = "true" ]; then
-    echo "Skipping prestart database checks and migrations as SKIP_PRESTART_DB is set to true"
-else
-    # Let the DB start
-    python app/backend_pre_start.py
-
-    # Run migrations
-    alembic upgrade head
-
-    # Create initial data in DB
-    python app/initial_data.py
-fi
+echo "Running prestart script (database initialization skipped)..."
