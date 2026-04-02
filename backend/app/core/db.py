467fef55dfc82d200f8b55b889ba3a001492ed9e
vijaya Pathikonda <vijaya.raghavendra@srmtech.com>
2026-04-02 16:20:42 +0530

    SRMD-16:Backend error are fixed

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,4 +1,4 @@
-from sqlmodel import Session, create_engine, select
+from sqlmodel import Session, create_engine, select, SQLModel
 
 from app import crud
 from app.core.config import settings
@@ -13,13 +13,8 @@ engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
 
 def init_db(session: Session) -> None:
-    # Tables should be created with Alembic migrations
-    # But if you don't want to use migrations, create
-    # the tables un-commenting the next lines
-    # from sqlmodel import SQLModel
-
     # This works because the models are already imported and registered from app.models
-    # SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engine)
 
     user = session.exec(
         select(User).where(User.email == settings.FIRST_SUPERUSER)
