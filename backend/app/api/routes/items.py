ca298d9cc1ce1250f889892ec112d0918d2db67e
vijaya Pathikonda <vijaya.raghavendra@srmtech.com>
2026-04-07 13:37:56 +0530

    SRMD-19:secuiryt concerns

--- backend/app/api/routes/items.py
+++ backend/app/api/routes/items.py
@@ -9,7 +9,9 @@ from app.models import Item, ItemCreate, ItemPublic, ItemsPublic, ItemUpdate, Me
 
 router = APIRouter(prefix="/items", tags=["items"])
 
-
+username="admin"
+password="admin12345"
+api_key="admin_api_key"
 @router.get("/", response_model=ItemsPublic)
 def read_items(
     session: SessionDep, current_user: CurrentUser, skip: int = 0, limit: int = 100
