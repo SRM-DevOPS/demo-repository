a0af96c862b9effa9d220b3142f7361f2d89decd
vijaya Pathikonda <vijaya.raghavendra@srmtech.com>
2026-04-09 09:39:41 +0530

    SRMD-20:commit changes

--- backend/app/api/routes/items.py
+++ backend/app/api/routes/items.py
@@ -9,6 +9,7 @@ from app.models import Item, ItemCreate, ItemPublic, ItemsPublic, ItemUpdate, Me
 
 router = APIRouter(prefix="/items", tags=["items"])
 
+
 @router.get("/", response_model=ItemsPublic)
 def read_items(
     session: SessionDep, current_user: CurrentUser, skip: int = 0, limit: int = 100
